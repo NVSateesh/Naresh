0ebadd7372fe4eae0d06860d37dd9235df8a9193
NVSateesh <sateesh.2276@gmail.com>
2015-09-26 23:15:34 +0530

    This comment is added from GIT-Pull this change to local
    This comment is added from GIT-Pull this change to local

--- GitProj/src/GitPac/NewTest2.java
+++ GitProj/src/GitPac/NewTest2.java
@@ -5,6 +5,8 @@ public class NewTest2 {
 	public static void main(String[] args) {
 		
 		System.out.println("New Test to git");
+		
+		System.out.println("This comment is added from GIT-Pull this change to local");
 
 	}
 
